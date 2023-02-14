a2b936a987952225cc2bf90b527115c47e98f91f
Ореолов Евгений <evoreo@yandex.ru>
2023-02-15 01:24:11 +0300

    Fixed

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -5,34 +5,36 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class CallbackTest {
+public class CallbackTest {
 
         WebDriver driver;
 
-        @BeforeAll
-        static void setupAll() {
-            System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
-        }
+    @BeforeAll
+    static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
 
-        @BeforeEach
-        void setup() {
-            driver = new ChromeDriver();
-        }
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.setHeadless(true);
 
-        @AfterEach
-        void teardown() {
-            driver.quit();
-            driver = null;
-        }
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:7777/");
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
 
         @Test
         void validOrder(){
-            driver.get("http://localhost:7777/");
             driver.findElement(By.cssSelector("[data-test-id=\"name\"] input")).sendKeys("Ореолов Евгений");
             driver.findElement(By.cssSelector("[data-test-id=\"phone\"] input")).sendKeys("+79999999999");
             driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
