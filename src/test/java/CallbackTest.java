b9795dc137227a2f69874b1a5755bc650076c6ca
Ореолов Евгений <evoreo@yandex.ru>
2023-02-15 16:05:24 +0300

    Final tests

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CallbackTest {
 
-        WebDriver driver;
+    WebDriver driver;
 
     @BeforeAll
     static void setupAll() {
@@ -33,14 +33,55 @@ public class CallbackTest {
         driver.quit();
     }
 
-        @Test
-        void validOrder(){
-            driver.findElement(By.cssSelector("[data-test-id=\"name\"] input")).sendKeys("Ореолов Евгений");
-            driver.findElement(By.cssSelector("[data-test-id=\"phone\"] input")).sendKeys("+79999999999");
-            driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
-            driver.findElement(By.cssSelector("[role=\"button\"]")).click();
-            String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-            String actual = driver.findElement(By.cssSelector("[data-test-id=\"order-success\"]")).getText().trim();
-            assertEquals (expected, actual);
-        }
+    @Test
+    void validOrder() {
+        driver.findElement(By.cssSelector("[data-test-id=\"name\"] input")).sendKeys("Ореолов Евгений");
+        driver.findElement(By.cssSelector("[data-test-id=\"phone\"] input")).sendKeys("+79999999999");
+        driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
+        driver.findElement(By.cssSelector("[role=\"button\"]")).click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=\"order-success\"]")).getText().trim();
+        assertEquals(expected, actual);
     }
+
+    @Test
+    void invalidPhone() {
+        driver.findElement(By.cssSelector("[data-test-id=\"name\"] input")).sendKeys("Ореолов Евгений");
+        driver.findElement(By.cssSelector("[data-test-id=\"phone\"] input")).sendKeys("999999999999");
+        driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
+        driver.findElement(By.cssSelector("[role=\"button\"]")).click();
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText().trim();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void noCheckBox() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Ореолов Евгений");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79999999999");
+        driver.findElement(By.cssSelector("[class=button__text]")).click();
+        String expected = "Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=agreement].input_invalid")).getText().trim();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void noName() {
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79999999999");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("[class=button__text]")).click();
+        String expected = "Поле обязательно для заполнения";
+        String actual = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText().trim();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void noPhone() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Ореолов Евгений");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("[class=button__text]")).click();
+        String expected = "Поле обязательно для заполнения";
+        String actual = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText().trim();
+        assertEquals(expected, actual);
+    }
+}
